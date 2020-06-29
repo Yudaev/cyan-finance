27e4255a612fbc7b9aa80c5fc6d37f61f75a0d74
ddevilcore <47120085+ddevilcore@users.noreply.github.com>
2020-06-29 18:45:06 +0400

    ProfilePages.jsx updated (classes)

--- client/src/components/ProfilePage/ProfilePage.jsx
+++ client/src/components/ProfilePage/ProfilePage.jsx
@@ -10,27 +10,27 @@ export default class Test extends Component {
     render() {
         return(
             <div className={cx("container", "content")}>
-                    <div className={cx("profile-header")}>
+                    <div className={cx("header")}>
                             <i className={cx("pi pi-user")}></i>
                     </div>
-                        <div className={cx("profile-settings")}>
-                                <p className="profile-settings__p">
+                        <div className={cx("settings")}>
+                                <p className="settings__p">
                                     user@mail.ru
                                 </p>
                                 <Button label="Change password" className="p-button-raised p-button-secondary" />
                         </div>
-                        <div className={cx("profile-body")}>
+                        <div className={cx("body")}>
 
                         </div>  
-                        <div className={cx("profile-footer")}>
-                            <div className={cx("profile-footer__social-links")}>
+                        <div className={cx("footer")}>
+                            <div className={cx("footer__social-links")}>
                                     <i className="pi pi-google"></i>
                                     <i className="pi pi-twitter"></i>
                                     <i className="pi pi-facebook"></i>
                                     <i className="pi pi-apple"></i>
                                     <i className="pi pi-android"></i>
                             </div>
-                            <div className={cx("profile-footer__exit")}>
+                            <div className={cx("footer__exit")}>
                                 <a href="#">Выход</a>
                             </div>
                         </div>
