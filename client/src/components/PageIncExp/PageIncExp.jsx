f921a3ac5882d1bbffc59bd75fbec5453a451cd6
JohnCarmack117 <wiirus.mv@yandex.ru>
2020-07-02 22:32:26 +0300

    #28 Главная - добавление расходов/доходов

--- client/src/components/PageIncExp/PageIncExp.jsx
+++ client/src/components/PageIncExp/PageIncExp.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import classnames from 'classnames/bind';
 
 import { InputNumber } from 'primereact/inputnumber';
-import { MultiSelect } from 'primereact/multiselect';
+import { AutoComplete } from 'primereact/autocomplete';
 import { SelectButton } from 'primereact/selectbutton';
 import styles from './PageIncExp.scss';
 
@@ -15,23 +15,53 @@ export default class Test extends Component {
         this.state = {
             valueNum: null,
             valueBut: null,
-            category: []
-        };        
-    }
+            selectedCategory: null,
+            filteredCategories: null
+        };  
+        
+        this.filterCategories = this.filterCategories.bind(this);
+        this.categoriesTemplate = this.categoriesTemplate.bind(this);
+
+    };
+
+    buttonsTemplate(option) {
+        return (
+            <div className={cx("incexp-body-button-temp")}>{ option.label }</div>
+        )
+    };
+
+    componentDidMount() {
+        this.categories = ['Автомобиль', 'Дети', 'Еда', 'Работа', 'Развлечения'];
+    };
+
+    filterCategories(event) {
+        setTimeout(() => {
+            let results;
+
+            if (event.query.length === 0) {
+                results = [...this.categories];
+            }
+            else {
+                results = this.categories.filter((category) => {
+                    return category.toLowerCase().startsWith(event.query.toLowerCase());
+                });
+            }
+
+            this.setState({ filteredCategories: results });
+        }, 250);
+    };
+
+    categoriesTemplate(categories) {
+        return (
+            <div style={{ fontSize: '16px' }}>{ categories }</div>  
+        );
+    };
 
     render() {
 
         const options = [
             {label: 'Доход', value: 'Income'},
             {label: 'Расход', value: 'Expense'}
-        ];
-
-        const categories = [
-            {label: 'Автомобиль', value: 'Car'},
-            {label: 'Дети', value: 'Kids'},
-            {label: 'Еда', value: 'Food'},
-            {label: 'Работа', value: 'Work'},
-            {label: 'Развлечения', value: 'Entertainment'}
         ];
 
         return(
@@ -43,6 +73,11 @@ export default class Test extends Component {
                         <span className={cx("incexp-header-span")}>Остаток</span>
                     </div>
                     <div className={cx("incexp-header-text")}>
+                        <span className={cx("incexp-header-span-opt")}>за день</span> 
+                        <span className={cx("incexp-header-span-opt")}>за месяц</span>
+                        <span className={cx("incexp-header-span-opt")}>за день</span>
+                    </div>
+                    <div className={cx("incexp-header-text")}>
                         <span className={cx("incexp-header-exp")}>900</span> 
                         <span className={cx("incexp-header-bud")}>516</span>
                         <span className={cx("incexp-header-bal")}>-384</span>
@@ -50,7 +85,8 @@ export default class Test extends Component {
                 </div>
                 <div className={cx("incexp-body")}>
                     <div className={cx("incexp-body-input")}>
-                        <InputNumber value={ this.state.valueNum } 
+                        <InputNumber 
+                            value={ this.state.valueNum } 
                             onChange={(e) => this.setState({valueNum: e.value})} 
                             mode="currency" 
                             currency="RUB" 
@@ -60,13 +96,16 @@ export default class Test extends Component {
                         />
                     </div>
                     <div className={cx("incexp-body-input")}>
-                        <MultiSelect 
-                            style={{ minWidth:'20.75em', minHeight:'2.2em' }}
+                        <AutoComplete 
                             placeholder={ 'Выберите категорию' }
-                            value={ this.state.category } 
-                            options={ categories } 
-                            onChange={ (e) => this.setState({category: e.value}) }
-                            filter={ true }
+                            size={32}
+                            value={ this.state.selectedCategory }
+                            suggestions={ this.state.filteredCategories }
+                            completeMethod={ this.filterCategories }
+                            dropdown={ true }
+                            style={{ height: '33px' }}
+                            itemTemplate={ this.categoriesTemplate }
+                            onChange={ (e) => this.setState({selectedCategory: e.value}) }
                         />
                     </div>
                     <div className={cx("incexp-body-button")}>
@@ -74,7 +113,8 @@ export default class Test extends Component {
                             style={{ fontWeight: 'bold' }}
                             value={ this.state.valueBut }
                             options={ options }
-                            onChange={ (e) => this.setState({valueBut: e.value}) }
+                            onChange={ (e) => this.setState({valueBut: e.value}) } 
+                            itemTemplate={ this.buttonsTemplate }
                         />
                     </div>
                 </div>
