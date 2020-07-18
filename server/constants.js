cc1e0dd93fe9d9514e07852ff32f413bc2f2bf0b
Roman Otstavnov <vatokato@gmail.com>
2020-07-18 11:34:15 +0500

    5f10940b8edd9c240f18a8ab #58 Добавление персональных категорий пользователя из пула сразу после регистрации
    * добавил файл с контстантами и занес туда стартовые категории подобно сервису monefy
    * добавил добавление категорий в базу при создании юзера

---
+++ server/constants.js
@@ -0,0 +1,17 @@
+module.exports = {
+  initialCategories: [
+    { title: 'Автомобиль' },
+    { title: 'Одежда' },
+    { title: 'Связь' },
+    { title: 'Еда' },
+    { title: 'Продукты' },
+    { title: 'Хозяйство' },
+    { title: 'Развлечения' },
+    { title: 'Подарки' },
+    { title: 'Здоровье' },
+    { title: 'Дом' },
+    { title: 'Животные' },
+    { title: 'Спорт' },
+    { title: 'Транспорт' },
+  ]
+};
