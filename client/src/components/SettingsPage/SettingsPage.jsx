3edeb6243edc31425951a0148ec44de71bb25287
Aleksandr <yudaev.a@me.com>
2020-08-10 02:06:07 +0300

    Восстановил страницу регулярных платежей

--- client/src/components/SettingsPage/SettingsPage.jsx
+++ client/src/components/SettingsPage/SettingsPage.jsx
@@ -6,41 +6,64 @@ import styles from './SettingsPage.scss';
 import { ScrollPanel } from 'primereact/scrollpanel';
 import { Button } from 'primereact/button';
 
-import Card from './Cards/Card.jsx';
-import EditOperationPage from '../EditOperationPage/EditOperationPage.jsx'
+import EditOperationPage from '../EditOperationPage/EditOperationPage'
+import OperationPageDateBlock from '../OperationsPage/OperationPageDateBlock/OperationPageDateBlock';
 
 const cx = classnames.bind(styles);
 
 class SettingsPage extends Component {
 
-    state = {
-        openPopUp: false
-    }
+  state = {
+    openPopUp: false
+  };
 
-    handlePopUp = ({}) => {
-        this.setState(prevState => ({
-            openPopUp: !prevState.openPopUp
-            })   
-        )
-    }
+  handlePopUp = () => {
+    this.setState(prevState => ({ openPopUp: !prevState.openPopUp }))
+  };
 
-    render() {
+  render() {
 
-        let show = this.state.openPopUp === true ? `${cx('popUp')}` : `${cx('noPopUp')}`
+    let show = this.state.openPopUp === true ? `${cx('popUp')}` : `${cx('noPopUp')}`;
 
-        return ( 
-            <div className={cx("container", "content")}>
-                <div className={show}>
-                  <EditOperationPage openPopUp={this.state.openPopUp} togglePopup={() => this.handlePopUp}/>
-                </div>
-                <ScrollPanel className={cx("cardList")}>
-                    <Card handlePopUp={this.handlePopUp} />
-                    <Card handlePopUp={this.handlePopUp} />
-                </ScrollPanel>
-                        <Button label="ДОБАВИТЬ ЗАПИСЬ" className="p-button-raised p-button-secondary" />
-            </div>   
-         )
-    }
+    const {
+      repetitiveOperations: repetitiveOperationsMap,
+      categories,
+      categoriesAsObject,
+      addRepetitiveOperation
+    } = this.props;
+    const repetitiveOperationsArray = [];
+    repetitiveOperationsMap.forEach((items, date) =>
+      repetitiveOperationsArray.push({ items, date })
+    );
+
+    return (
+      <div className={cx("container", "content")}>
+        <div className={show}>
+          <EditOperationPage
+            togglePopup={this.handlePopUp}
+            categories={categories}
+            addRepetitiveOperation={addRepetitiveOperation}
+          />
+        </div>
+        <ScrollPanel className={cx("cardList")}>
+          {repetitiveOperationsArray.map(({ items, date }, key) =>
+            (<OperationPageDateBlock
+              key={key}
+              handlePopUp={this.handlePopUp}
+              items={items}
+              date={date}
+              categories={categoriesAsObject}
+            />)
+          )}
+        </ScrollPanel>
+        <Button
+          label="ДОБАВИТЬ ЗАПИСЬ"
+          className="p-button-raised p-button-secondary"
+          onClick={() => this.handlePopUp()}
+        />
+      </div>
+    )
+  }
 }
 
 export default SettingsPage;
